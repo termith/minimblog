1ddacf50d175cb41a3b054092eb927cd349ee5c7
Dmitry Demidov <dmitdemidov@gmail.com>
2017-08-23 19:49:10 +0300

    Create logging package

--- common/logging/logging.go
+++ common/logging/logging.go
@@ -1,3 +1,37 @@
 package logging
 
-import "github.com/sirupsen/logrus"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/termith/minimblog/common/config"
+)
+
+var (
+	Info  *log.Logger
+	Warn  *log.Logger
+	Error *log.Logger
+)
+
+func InitLogs(cfg config.Config, mode string) {
+	logFlags := log.Ldate | log.Ltime | log.Lshortfile
+	logFile, err := os.OpenFile(cfg.Logging.File, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Println("Error while open logfile")
+		os.Exit(1)
+	}
+	Info = log.New(os.Stdout, "INFO: ", logFlags)
+	Warn = log.New(os.Stdout, "WARNING: ", logFlags)
+	Error = log.New(logFile, "ERROR: ", logFlags)
+
+	switch mode {
+	case "production":
+		Info.SetOutput(ioutil.Discard)
+		Warn.SetOutput(ioutil.Discard)
+	case "development":
+		Info.SetOutput(logFile)
+	}
+
+}
--- engine/minimblog.go
+++ engine/minimblog.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/termith/minimblog/common/config"
+	"github.com/termith/minimblog/common/logging"
 	"github.com/termith/minimblog/engine/handlers"
 )
 
-func main() {
-	configuration := config.LoadConfig("/etc/minimblog/config.json")
-	addr := fmt.Sprintf(":%d", configuration.Application.Port)
+var mode string
 
+func main() {
+
+	flag.StringVar(&mode, "mode", "development", "Run mode: production or development")
+	flag.Parse()
+
+	if mode != "production" || mode != "development" {
+		fmt.Println("Mode should be one of (production, development)")
+		os.Exit(1)
+	}
+	configuration := config.LoadConfig("/etc/minimblog/config.json")
+
+	logging.InitLogs(configuration, mode)
+	addr := fmt.Sprintf(":%d", configuration.Application.Port)
 	http.HandleFunc("/post/", handlers.PostHandler)
 	http.HandleFunc("/", handlers.RootHandler)
+
 	http.ListenAndServe(addr, nil)
 }
