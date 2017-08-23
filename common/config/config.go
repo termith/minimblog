631f2036a2f74f749d079df8de66bf6cfc4395f0
Dmitry Demidov <dmitdemidov@gmail.com>
2017-08-23 13:43:06 +0300

    Add config.json and configuration parsing

--- common/config/config.go
+++ common/config/config.go
@@ -1,1 +1,43 @@
 package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Config struct {
+	Logging struct {
+		Levels struct {
+			Production  string `json:"production"`
+			Development string `json:"development"`
+		} `json:"levels"`
+		File string `json:"file"`
+	} `json:"logging"`
+	Database struct {
+		Driver string `json:"driver"`
+	} `json:"database"`
+	Application struct {
+		Port int `json:"port"`
+	} `json:"application"`
+}
+
+func LoadConfig(filePath string) *Config {
+	/*
+		Loads configuration from filePath
+	*/
+	var config Config
+	rawFile, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("Error while read config file")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	err = json.Unmarshal(rawFile, &config)
+	if err != nil {
+		fmt.Println("Error while unmarshall json")
+		fmt.Println(err.Error())
+	}
+	return &config
+}
--- engine/minimblog.go
+++ engine/minimblog.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/termith/minimblog/common/config"
 	"github.com/termith/minimblog/engine/handlers"
 )
 
 func main() {
+	configuration := config.LoadConfig("/etc/minimblog/config.json")
+	addr := fmt.Sprintf(":%d", configuration.Application.Port)
+
 	http.HandleFunc("/post/", handlers.PostHandler)
 	http.HandleFunc("/", handlers.RootHandler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(addr, nil)
 }
