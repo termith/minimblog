de414f94e9d8f894f3ec5a0fea1f61cdcaf70a2d
Dmitry Demidov <dmitdemidov@gmail.com>
2017-09-05 00:15:35 +0300

    Improve test_basic

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 @pytest.yield_fixture(scope="module")
 def run_engine():
-    os.system('../minimblog')
+    os.system('minimblog')
     print('Engine has been started')
     yield
     print('Stoping engine...')
