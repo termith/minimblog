120fd823a3d6699625f795ae2df215704880c5da
Dmitry Demidov <dmitdemidov@gmail.com>
2017-09-05 09:44:01 +0300

    Fix tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,5 @@
 import os
+import threading
 import subprocess
 
 
@@ -6,6 +7,9 @@ def test_engine_running():
     """
         Check that engine is running
     """
-    os.system('minimblog')
+    minimblog = threading.Thread(target=lambda: os.system('minimblog'))
+    minimblog.start()
+    print('Engine started...')
     assert 'minimblog' in str(subprocess.check_output(['ps', 'ax']))
     os.system('killall minimblog')
+    minimblog.join()
