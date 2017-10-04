3c1fa4bdc00a2e9e77eedff679a761f6ad05244c
Dmitry Demidov <dmitdemidov@gmail.com>
2017-10-04 09:41:42 +0300

    Add dao methods prototypes

--- common/model/post/dao.go
+++ common/model/post/dao.go
@@ -1,5 +1,21 @@
 package post
 
 const (
-	CREATE_POST = "INSERT INTO posts (title, text, created) VALUES ($1, $2, $3); SELECT last_insert_rowid() from posts;"
+	CREATE_POST    = "INSERT INTO posts (title, text, created) VALUES ($1, $2, $3);"
+	GET_LAST_POSTS = "SELECT title, text, created FROM posts LIMIT $1"
 )
+
+func GetLastPosts(count int) ([]*Post, error) {
+	/*
+		Return %count% of last posts from DB
+	*/
+
+}
+
+func CreatePost(created int, title, text string) error {
+	/*
+		Create post with %title%, %text% created at %created% (Unix time)
+	*/
+	var err error
+	return err
+}
